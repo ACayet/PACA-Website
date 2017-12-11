9abe8bf6879886f14f743a045db9fe98d6fbc754
TOM MOORE <moore@iut.univ-paris5.fr>
2017-12-11 15:52:10 +0100

    Ajout du template

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 jQuery(function () {
+
+
+    $("body").css("display", "none");
+
+    $("body").fadeIn(2000);
+
+    $("a.transition").click(function (event) {
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(5000, redirectPage);
+    });
+
+    function redirectPage() {
+        window.location = linkLocation;
+    }
+
+
     $("#avignon").click(function () {
         location.href = "avignon.html";
     });
@@ -8,7 +25,7 @@ jQuery(function () {
     });
 
     $("#cannes").click(function () {
-        location.href = "cannes.html";
+        location.href = "cannes.html#debutpage";
     });
 
     $("#marseille").click(function () {
@@ -18,9 +35,26 @@ jQuery(function () {
     $("#nice").click(function () {
         location.href = "nice.html";
     });
-    
+
     $("#voirPlus").click(function () {
         location.href = "voirPlus.html";
+    });
+
+    $('a').on('click', function (evt) {
+        // bloquer le comportement par défaut: on ne rechargera pas la page
+        
+        // enregistre la valeur de l'attribut  href dans la variable target
+        var target = $(this).attr('href');
+        /* le sélecteur $(html, body) permet de corriger un bug sur chrome 
+        et safari (webkit) */
+        $('html, body')
+            // on arrête toutes les animations en cours 
+            .stop()
+            /* on fait maintenant l'animation vers le haut (scrollTop) vers 
+             notre ancre target */
+            .animate({
+                scrollTop: $(target).offset().top
+            }, 1000);
     });
 
 
